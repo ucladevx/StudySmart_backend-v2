903c23e9c7fa916aeaf13f0b590aa5d250ed53dd
aseem191 <aseem191@gmail.com>
2018-11-10 23:52:59 -0800

    Added basic framework

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+var origin = "https://google.com";
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', origin || "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+    next();
+});
+
+app.get('/', function(req, res){
+	res.send("StudySmart");
+})
