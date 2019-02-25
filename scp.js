77e17fedeb796e4e7399af2d015b0710c98233fc
Anurang Bhuta <anurangbhuta@s-164-67-214-62.resnet.ucla.edu>
2019-02-24 17:36:59 -0800

    exported successfully started pushing to db and created the foundation for scheduling jobs

--- scp.js
+++ scp.js
@@ -51,90 +51,96 @@ const request = require('request');
     }
     ]
 }*/
-(async function main() {
-  
-    try {
-        
-        const browser = await puppeteer.launch();
-        const [page] = await browser.pages();
-        //Url to visit
-        await page.goto('https://www.library.ucla.edu/hours');
+module.exports = {
+    test: async function () {
+        try {
+            // console.log("here");
+            console.log("here");
+            const browser = await puppeteer.launch();
+            const [page] = await browser.pages();
+            //Url to visit
+            await page.goto('https://www.library.ucla.edu/hours');
 
-        const result = await page.evaluate(async () => {
-            await new Promise((resolve) => setTimeout(resolve, 1500));
-            //find names of the 11 libraries
-            const names=document.querySelectorAll('div.pane-node-title h2')
-            //find address of the 11 libraries
-            const addresses=document.querySelectorAll('div.location__address')
-            //find opening times of the 11 libraries
-            const time_of_dp=document.querySelectorAll('table.opening-hours-week')
-            //find phone number
-            const phones = document.querySelectorAll('span.location__phone-number')
-            //find image
-            const imgs=document.querySelectorAll('img')
-            const num_rooms = names.length;
-            const data = [];
-            for (let n = 0; n < num_rooms; n++)
-            {
-                var obj = new Object();
-                //add name to lib obj
-                const lib_name=names[n].innerText
-                obj["name"]=lib_name
-               
-                //add location to lib obj
-                const lib_loc=addresses[n].innerText
-                obj["location"]=lib_loc
-                //add phone-number to lib obj
-                const lib_phone=phones[n].innerText
-                obj["phone"]=lib_phone
-                //add image to lib obj
-                const img=imgs[n+1].src
-                obj["image"]=img                
-                //add department to lib obj
-                const department=[]
-                lib_dp = time_of_dp[n].innerText
-                list_dp=lib_dp.split("\n")
-                //first line : date 
-                //after : openning time for each department
-                const date = list_dp[0].split('\t')
-                obj["start_date"]=date[0].split("-")[0]
-                const num_dp=list_dp.length-1
-                for(let i = 0;i<num_dp;i++)
-                {
-                    var dp_obj = new Object();
-                    dp_info=list_dp[i+1].split('\t')
-                    dp_obj["department_name"]=dp_info[0]
+            const result = await page.evaluate(async () => {
+                await new Promise((resolve) => setTimeout(resolve, 1500));
+                //find names of the 11 libraries
+                const names = document.querySelectorAll('div.pane-node-title h2')
+                //find address of the 11 libraries
+                const addresses = document.querySelectorAll('div.location__address')
+                //find opening times of the 11 libraries
+                const time_of_dp = document.querySelectorAll('table.opening-hours-week')
+                //find phone number
+                const phones = document.querySelectorAll('span.location__phone-number')
+                //find image
+                const imgs = document.querySelectorAll('img')
+                const num_rooms = names.length;
+                const data = [];
+                for (let n = 0; n < num_rooms; n++) {
+                    var obj = new Object();
+                    //add name to lib obj
+                    const lib_name = names[n].innerText
+                    obj["name"] = lib_name
 
-                    //console.log(list_dp[i+1].split('\t'));
-                    //dp_open=dp_info[1].split("  ")
-                    //dp_obj["dp_info"]=dp_info[2]
-                    dp_open_time=[]
-                    const open_day=dp_info.length-1
-                    for(let j=0;j<open_day;j++)
-                    {
-                        var dp_time_obj = new Object();
-                        dp_time_obj["dp_open_time"]=dp_info[j+1]
-                        dp_time_obj["date"]=date[j+1]
-                        dp_open_time.push(dp_time_obj); 
+                    //add location to lib obj
+                    const lib_loc = addresses[n].innerText
+                    obj["location"] = lib_loc
+                    //add phone-number to lib obj
+                    const lib_phone = phones[n].innerText
+                    obj["phone"] = lib_phone
+                    //add image to lib obj
+                    const img = imgs[n + 1].src
+                    obj["image"] = img
+                    //add department to lib obj
+                    const department = []
+                    lib_dp = time_of_dp[n].innerText
+                    list_dp = lib_dp.split("\n")
+                    //first line : date 
+                    //after : openning time for each department
+                    const date = list_dp[0].split('\t')
+                    obj["start_date"] = date[0].split("-")[0]
+                    const num_dp = list_dp.length - 1
+                    for (let i = 0; i < num_dp; i++) {
+                        var dp_obj = new Object();
+                        dp_info = list_dp[i + 1].split('\t')
+                        dp_obj["department_name"] = dp_info[0]
+
+                        //console.log(list_dp[i+1].split('\t'));
+                        //dp_open=dp_info[1].split("  ")
+                        //dp_obj["dp_info"]=dp_info[2]
+                        dp_open_time = []
+                        const open_day = dp_info.length - 1
+                        for (let j = 0; j < open_day; j++) {
+                            var dp_time_obj = new Object();
+                            dp_time_obj["dp_open_time"] = dp_info[j + 1]
+                            dp_time_obj["date"] = date[j + 1]
+                            dp_open_time.push(dp_time_obj);
+                        }
+                        dp_obj["time"] = dp_open_time
+                        department.push(dp_obj);
                     }
-                    dp_obj["time"]=dp_open_time
-                    department.push(dp_obj); 
+                    obj["department"] = department
+                    data.push(obj);
                 }
-                obj["department"]=department
-                data.push(JSON.stringify(obj));  
-            }
-            return data
-        
-        });
 
-        console.log(result);
-        await browser.close();
-    } 
-    
-    //If there is an error, write to console and exit
-    catch (err) {
-        console.error(err);
-        return;
+                return data
+
+            });
+
+            // console.log(result);
+            request.post({ url: "http://studysmart-env-2.dqiv29pdi2.us-east-1.elasticbeanstalk.com/libinfo", headers: { 'content-type': 'application/json' }, body: JSON.stringify(result) }, function (err, response, body) {
+                console.log(response.body)
+                //console.log(JSON.stringify(response.body))
+            })
+            await browser.close();
+            return;
+        }
+
+        //If there is an error, write to console and exit
+        catch (err) {
+            console.error(err);
+            return;
+        }
+
     }
-
-})();
+}
+// test();
