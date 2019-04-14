82894868104ccc1c2655bbfeca6e9407c1d4a735
Arihant Jain <arihat72600@gmail.com>
2019-04-13 18:14:10 -0700

    made webscraper faster

--- webscraper.js
+++ webscraper.js
@@ -32,16 +32,13 @@ const formatString = "YYYY-MM-DD";
 let next21DaysStrings = [];
 
 let numSaturdays = 0;
+
 for (var i = 0; numSaturdays < 3; i++) {
   next21DaysStrings[i] = now.format(formatString);
 
   if (now.day() == 6) numSaturdays++;
   now.add(1, "days");
 }
-
-var errors = 0;
-
-let timeout = 0;
 
 for (
   var meetingRoomIndex = 0;
@@ -53,31 +50,57 @@ for (
     durationIndex < durations.length;
     durationIndex++
   ) {
-    for (var dayIndex = 0; dayIndex < next21DaysStrings.length; dayIndex++) {
-      sendRequest(meetingRoomIndex, durationIndex, dayIndex, timeout);
-      ++timeout;
-    }
+    for (var dayIndex = 0; dayIndex < next21DaysStrings.length; dayIndex++)
+      sendRequest(meetingRoomIndex, durationIndex, dayIndex);
   }
 }
 
-function sendRequest(a, b, c, d) {
+function sendRequest(meetingRoomIndex, durationIndex, dayIndex) {
   let options = {
     method: "GET",
     url: "http://reslife.ucla.edu/reserve",
     qs: {
-      type: meetingRooms[a],
-      duration: durations[b],
-      date: next21DaysStrings[c],
+      type: meetingRooms[meetingRoomIndex],
+      duration: durations[durationIndex],
+      date: next21DaysStrings[dayIndex],
       partial: ""
     },
     headers: {},
     callback: function(error, response, body) {
-      if (error) {
-        ++errors;
-        console.log(errors);
-        sendRequest(a, b, c, 0);
-      } else {
-        console.log("worked correctly");
+      if (error) sendRequest(meetingRoomIndex, durationIndex, dayIndex);
+      else {
+        const dom = new JSDOM(body);
+
+        let data = [];
+        let columns = dom.window.document.querySelectorAll(".col-md-6");
+        //Get all links so that you can redirect user to registration page directly
+        let links = dom.window.document.getElementsByTagName("a");
+        let filteredLinks = [];
+        //Need to filter all links so that we only save the ones with the class mentioned below
+        for (k = 0; k < links.length; k++) {
+          if (
+            links[k].getAttribute("class") ==
+            "btn btn-sm btn-block btn-default btn-select"
+          ) {
+            filteredLinks.push(links[k]);
+          }
+        }
+        //Counter for filtered links
+        var k = 0;
+        //Start at 2 because first value is not relevant
+        //Increment by 2 because every pair of 2 elements gives us "room details" and "time"
+
+        for (var i = 2; i < columns.length; i += 2) {
+          //Create JS object and push required elements
+          var obj = new Object();
+          obj["Room Details"] = columns[i].textContent.trim();
+          obj["Time"] = columns[i + 1].textContent.trim();
+          obj["Link"] = filteredLinks[k].getAttribute("href");
+          k++;
+          //Push
+          data.push(obj);
+        }
+        console.log(data);
       }
     }
   };
